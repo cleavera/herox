5f57d343ae4d4670115a5f9afd527b4ca20e17fd
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2025-08-06 14:53:12 +0100

    Add global key listener implementation

--- src/global_listener.rs
+++ src/global_listener.rs
@@ -1,72 +1,135 @@
 use napi::bindgen_prelude::*;
-use napi::threadsafe_function::ThreadsafeFunction;
+use napi::threadsafe_function::{ThreadsafeFunction, ThreadsafeFunctionCallMode};
 use std::collections::HashMap;
-use std::sync::mpsc::Sender;
+use std::sync::mpsc::{channel, sync_channel, Sender};
 use std::sync::{Arc, Mutex};
-use std::thread::JoinHandle;
+use std::thread::{self, JoinHandle};
+
+pub mod unsupported_backend;
+pub mod windows_backend;
 
 #[napi]
 #[derive(Clone, Copy, Debug)]
 pub enum GlobalInputAction {
-    Keyboard,
+  Keyboard,
 }
 
-// A thread-safe function that can be called from any thread to notify JS.
 type Subscriber = ThreadsafeFunction<GlobalInputAction>;
-// A unique ID to track and remove subscribers.
 type SubscriberId = u64;
 
-// The shared state that will be accessed by the listener and dispatcher threads.
 #[derive(Clone, Default)]
 struct ListenerState {
-    subscribers: Arc<Mutex<HashMap<SubscriberId, Subscriber>>>,
-    next_id: Arc<Mutex<SubscriberId>>,
+  subscribers: Arc<Mutex<HashMap<SubscriberId, Subscriber>>>,
+  next_id: Arc<Mutex<SubscriberId>>,
+}
+
+impl ListenerState {
+  fn add_subscriber(&self, subscriber: Subscriber) -> SubscriberId {
+    let mut next_id_guard = self.next_id.lock().unwrap();
+    let id = *next_id_guard;
+    *next_id_guard += 1;
+
+    let mut subs_guard = self.subscribers.lock().unwrap();
+    subs_guard.insert(id, subscriber);
+    id
+  }
+
+  fn remove_subscriber(&self, id: SubscriberId) {
+    let mut subs_guard = self.subscribers.lock().unwrap();
+    subs_guard.remove(&id);
+  }
+
+  fn broadcast(&self, action: GlobalInputAction) {
+    let subs_guard = self.subscribers.lock().unwrap();
+    for sub in subs_guard.values() {
+      sub.call(Ok(action), ThreadsafeFunctionCallMode::Blocking);
+    }
+  }
 }
 
 #[napi]
 pub struct GlobalListener {
-    // Sends actions from the OS listener thread to the dispatcher thread.
-    action_tx: Option<Sender<GlobalInputAction>>,
-    // We hold the handles to ensure threads are cleaned up when the listener is dropped.
-    _os_listener_handle: Option<JoinHandle<()>>,
-    _dispatcher_handle: Option<JoinHandle<()>>,
+  state: ListenerState,
+  action_tx: Option<Sender<GlobalInputAction>>,
+  _os_listener_handle: Option<JoinHandle<()>>,
+  _dispatcher_handle: Option<JoinHandle<()>>,
 }
 
 #[napi]
 impl GlobalListener {
-    #[napi(constructor)]
-    pub fn new() -> Self {
-        // 1. Create a new mpsc channel (sender/receiver).
-        // 2. Create a new ListenerState.
-        // 3. Spawn the dispatcher thread, giving it the receiver and state.
-        // 4. Spawn the OS listener thread, giving it the sender.
-        // 5. Return a new `GlobalListener` instance containing the sender and thread handles.
-        todo!()
-    }
+  #[napi(constructor)]
+  pub fn new() -> Result<Self> {
+    let state = ListenerState::default();
+    let (action_tx, action_rx) = channel::<GlobalInputAction>();
 
-    /// Subscribes to global input events.
-    ///
-    /// Returns a function that, when called, will unsubscribe the listener.
-    #[napi]
-    pub fn subscribe(
-        &self,
-        _env: Env,
-        #[napi(ts_arg_type = "(action: GlobalInputAction) => void")] _subscriber: ThreadsafeFunction<GlobalInputAction>,
-    ) -> Result<Function<()>> {
-        // 1. Get a new unique ID.
-        // 2. Store the `subscriber` (ThreadsafeFunction) in the shared state (HashMap).
-        // 3. Return a new JS function that, when called, removes the subscriber from the HashMap using its ID.
-        todo!()
-    }
+    let dispatcher_state = state.clone();
+    let _dispatcher_handle = Some(thread::spawn(move || {
+      while let Ok(action) = action_rx.recv() {
+        dispatcher_state.broadcast(action);
+      }
+    }));
 
-    /// Closes the listener and shuts down the background threads gracefully.
-    #[napi]
-    pub fn close(&mut self) -> Result<()> {
-        // 1. Take ownership of the `action_tx` sender.
-        // 2. Drop the sender. This closes the channel, causing the `recv()` calls in the
-        //    dispatcher and OS listener threads to return an `Err`, gracefully exiting their loops.
-        // 3. Set the thread handles to `None`.
-        todo!()
+    let os_listener_tx = action_tx.clone();
+    let (init_tx, init_rx) = sync_channel(1);
+
+    let _os_listener_handle = Some(thread::spawn(move || {
+      #[cfg(target_os = "windows")]
+      windows_backend::start_listener(os_listener_tx, init_tx);
+
+      #[cfg(not(target_os = "windows"))]
+      unsupported_backend::start_listener(os_listener_tx, init_tx);
+    }));
+
+    match init_rx.recv() {
+      Ok(Ok(())) => {
+        Ok(Self {
+          state,
+          action_tx: Some(action_tx),
+          _os_listener_handle,
+          _dispatcher_handle,
+        })
+      }
+      Ok(Err(err_msg)) => {
+        Err(Error::from_reason(err_msg))
+      }
+      Err(_) => {
+        Err(Error::from_reason(
+          "The global listener thread panicked during initialization.",
+        ))
+      }
     }
+  }
+
+  #[napi]
+  pub fn subscribe<'a>(
+    &'a self,
+    env: &'a Env,
+    #[napi(ts_arg_type = "(action: GlobalInputAction) => void")] subscriber: ThreadsafeFunction<
+      GlobalInputAction,
+    >,
+  ) -> Result<Function<'a, (), ()>> {
+    let id = self.state.add_subscriber(subscriber);
+    let state_clone = self.state.clone();
+
+    env.create_function_from_closure("unsubscribe", move |_ctx| {
+      state_clone.remove_subscriber(id);
+      Ok(())
+    })
+  }
+
+  #[napi]
+  pub fn close(&mut self) -> Result<()> {
+    if let Some(tx) = self.action_tx.take() {
+      drop(tx);
+    }
+    self._os_listener_handle = None;
+    self._dispatcher_handle = None;
+    Ok(())
+  }
 }
 
+impl Drop for GlobalListener {
+  fn drop(&mut self) {
+    let _ = self.close();
+  }
+}
