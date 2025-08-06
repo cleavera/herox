725c4687c1573e08c467b3704386c1cb37a5a715
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2025-08-06 15:02:21 +0100

    Code formatting

--- src/global_listener/unsupported_backend.rs
+++ src/global_listener/unsupported_backend.rs
@@ -3,8 +3,9 @@
 use crate::global_listener::GlobalInputAction;
 use std::sync::mpsc::{Sender, SyncSender};
 
-// On non-Windows platforms, the listener is not supported.
-pub fn start_listener(_tx: Sender<GlobalInputAction>, init_tx: SyncSender<Result<(), &'static str>>) {
-    // Signal that this platform is unsupported.
-    let _ = init_tx.send(Err("Global listener is not supported on this platform."));
+pub fn start_listener(
+  _tx: Sender<GlobalInputAction>,
+  init_tx: SyncSender<Result<(), &'static str>>,
+) {
+  let _ = init_tx.send(Err("Global listener is not supported on this platform."));
 }
